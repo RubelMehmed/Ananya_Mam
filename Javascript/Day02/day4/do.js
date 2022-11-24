3c44d9ae45c6ec18c737d9edd18daf7182f3aea1
RubelMehmed <rubel.49@geography-juniv.edu.bd>
2022-11-24 12:47:55 +0600

    Arithmatic operator

---
+++ Javascript/Day02/day4/do.js
@@ -0,0 +1,13 @@
+// Arithmatic Operators
+
+let a = 10;
+let b = 20;
+
+document.write("<br> Arithmatic: " + (a+b));
+document.write("<br> a++ : " + (a++));
+document.write("<br> b++ : " + (b++));
+b=a++;   
+document.write("<br>a: " + (+ a));
+document.write("<br>b: " + +b);
+
+//
